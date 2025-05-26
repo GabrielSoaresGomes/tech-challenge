00b5e3ef08e5a3d6c55a21e4db99073e6139b097
Junior <junior.ng@outlook.com.br>
2025-05-26 00:20:28 -0300

    feat: adiciona configuração do Swagger

--- src/main/java/com/postech/challenge_01/config/OpenApiConfig.java
+++ src/main/java/com/postech/challenge_01/config/OpenApiConfig.java
@@ -1,4 +1,25 @@
 package com.postech.challenge_01.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tech Challenge 1 - Gerenciamento de usuários, endereços e autenticação de usuários")
+                        .description("Esta API RESTful permite buscar, adicionar, atualizar e deletar os usuários e endereços, também permite a autenticação de usuários.")
+                        .version("v1.0.0")
+                        .license(new License()
+                                .name("Uso acadêmico - Tech Challenge")
+                                .url("https://github.com/GabrielSoaresGomes/tech-challenge-01")
+                        )
+                );
+    }
 }
