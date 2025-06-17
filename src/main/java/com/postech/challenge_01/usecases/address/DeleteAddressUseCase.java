dfbff433cf80b9b98e636a52da3c9739b02cac13
Gabriel Soares <gabrielsoares221@gmail.com>
2025-06-17 00:51:37 -0300

    fix: Correção do repository do delete no address

--- src/main/java/com/postech/challenge_01/usecases/address/DeleteAddressUseCase.java
+++ src/main/java/com/postech/challenge_01/usecases/address/DeleteAddressUseCase.java
@@ -1,7 +1,7 @@
 package com.postech.challenge_01.usecases.address;
 
 import com.postech.challenge_01.exceptions.ResourceNotFoundException;
-import com.postech.challenge_01.repositories.UserRepository;
+import com.postech.challenge_01.repositories.AddressRepository;
 import com.postech.challenge_01.usecases.UseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class DeleteAddressUseCase implements UseCase<Long, Void> {
-    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
 
     @Override
     public Void execute(Long id) {
         log.info("Deletando endereço com ID: {}", id);
-        Integer delete = this.userRepository.delete(id);
+        Integer delete = this.addressRepository.delete(id);
 
         if (delete == 0) {
             throw new ResourceNotFoundException("Endereço com ID " + id + " não foi encontrado");
