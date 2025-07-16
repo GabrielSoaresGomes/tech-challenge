05c4b4f54166e279ebb4c808c314ed82c23cf58a
Bruno Bender <brunopaulobender@gmail.com>
2025-07-16 17:10:47 -0300

    [FEAT][NO-ISSUE]: Importando bibliotecas de teste e criação de estrutura com teste de exemplo.

---
+++ src/test/java/com/postech/challenge_01/usecases/user/SaveUserUseCaseTest.java
@@ -0,0 +1,67 @@
+package com.postech.challenge_01.usecases.user;
+
+import com.postech.challenge_01.domains.User;
+import com.postech.challenge_01.dtos.requests.UserRequestDTO;
+import com.postech.challenge_01.dtos.responses.UserResponseDTO;
+import com.postech.challenge_01.repositories.UserRepository;
+import com.postech.challenge_01.usecases.rules.Rule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class SaveUserUseCaseTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private Rule<User> ruleMock;
+
+    @InjectMocks
+    private SaveUserUseCase saveUserUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        saveUserUseCase = new SaveUserUseCase(userRepository, passwordEncoder, List.of(ruleMock));
+    }
+
+    @Test
+    void shouldExecuteAndSaveUserSuccessfully() {
+        // Arrange
+        var id = 1L;
+        var name = "Nome Teste";
+        var email = "teste@teste.com";
+        var login = "teste.teste";
+        var password = "teste123";
+        var encodedPassword = "encodedPassword123";
+
+        UserRequestDTO request = new UserRequestDTO(name, email, login, password);
+        User savedUser = new User(id, name, email, login, password);
+
+        when(passwordEncoder.encode(request.password())).thenReturn(encodedPassword);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        //Act
+        UserResponseDTO response = saveUserUseCase.execute(request);
+
+        //Assert
+        verify(passwordEncoder, times(1)).encode(request.password());
+        verify(ruleMock).execute(any(User.class));
+        verify(userRepository, times(1)).save(any(User.class));
+
+        assertThat(response).isNotNull();
+        assertThat(response.id()).isEqualTo(id);
+        assertThat(response.name()).isEqualTo(name);
+        assertThat(response.email()).isEqualTo(email);
+        assertThat(response.login()).isEqualTo(login);
+    }
+
+}
