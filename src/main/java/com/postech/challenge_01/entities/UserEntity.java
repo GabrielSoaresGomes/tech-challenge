14e9f34903c7a0b362558554172fd81337ae0a43
Gabriel Soares <gabrielsoares221@gmail.com>
2025-07-25 00:44:28 -0300

    [FEAT][ISSUE#38]: Adicionando anotações do JPA na entidade User

--- src/main/java/com/postech/challenge_01/entities/UserEntity.java
+++ src/main/java/com/postech/challenge_01/entities/UserEntity.java
@@ -1,8 +1,10 @@
 package com.postech.challenge_01.entities;
 
 import com.postech.challenge_01.domains.User;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
@@ -11,14 +13,37 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
-public class UserEntity {
+@Entity
+@Table(name = "users")
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
     private Long userTypeId;
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(nullable = false, unique = true, length = 50)
     private String login;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private LocalDateTime lastModifiedDateTime;
+
+    @PrePersist
+    @PreUpdate
+    public void updateLastModifiedDateTime() {
+        this.lastModifiedDateTime = LocalDateTime.now();
+    }
 
     public static UserEntity of(final User user) {
         return new UserEntity(
